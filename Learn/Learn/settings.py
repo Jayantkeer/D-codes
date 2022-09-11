8dab186afb4cc93a02febe63c51b0ae13d988af1
Jayantkeer <50320475+Jayantkeer@users.noreply.github.com>
2022-09-11 06:33:42 -0700

    Update settings.py
    commit for security

--- Learn/Learn/settings.py
+++ Learn/Learn/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z$y8a5yw*=&693q1el^rznhg5j8z6n!1ao@&3kxk%bn4@_(d6='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
